5ca085c7d2da7b40a7cd0d0efa29d0dbfe7f7f9b
Roberto Sousa <robertosousa186@gmail.com>
2021-01-06 21:42:51 -0200

    Segundo teste passando(GREEN)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const FizzBuzz = (num) => {
   if (num % 3 === 0) return 'Fizz';
+  if (num % 5 === 0) return 'Buzz';
 };
 
 export default FizzBuzz;
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -5,9 +5,11 @@ describe('FizzBuzz', () => {
 
   it('should return `Fizz` when multiple of 3', () => {
     expect(FizzBuzz(3)).to.be.equal('Fizz');
+    expect(FizzBuzz(6)).to.be.equal('Fizz');
   });
 
   it('should return `Buzz` when multiple of 5', () => {
     expect(FizzBuzz(5)).to.be.equal('Buzz');
+    expect(FizzBuzz(10)).to.be.equal('Buzz');
   })
 });
