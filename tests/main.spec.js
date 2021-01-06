5a5a27a1977a02e5c70b618d9155cd122a461fc9
Roberto Sousa <robertosousa186@gmail.com>
2021-01-06 21:03:59 -0200

    Segundo teste não passa(RED)

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,9 +1,13 @@
 import { expect } from 'chai';
 import FizzBuzz from '../src/main';
 
-describe('Main', () => {
+describe('FizzBuzz', () => {
 
-  it('should return `Fizz` when multiple for 3', () => {
+  it('should return `Fizz` when multiple of 3', () => {
     expect(FizzBuzz(3)).to.be.equal('Fizz');
   });
+
+  it('should return `Buzz` when multiple of 5', () => {
+    expect(FizzBuzz(5)).to.be.equal('Buzz');
+  })
 });
