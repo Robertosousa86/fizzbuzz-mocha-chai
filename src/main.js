217046b61155753e4cb23d79b378af9cd48e9f4a
Roberto Sousa <robertosousa186@gmail.com>
2021-01-06 22:00:28 -0200

    Terceiro teste passando(Green)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const FizzBuzz = (num) => {
+  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
   if (num % 3 === 0) return 'Fizz';
   if (num % 5 === 0) return 'Buzz';
+  // if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz'; se o if for colocado aqui ocorrerá um erro.
 };
 
 export default FizzBuzz;
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -11,5 +11,9 @@ describe('FizzBuzz', () => {
   it('should return `Buzz` when multiple of 5', () => {
     expect(FizzBuzz(5)).to.be.equal('Buzz');
     expect(FizzBuzz(10)).to.be.equal('Buzz');
+  });
+
+  it('should return `FizzBuzz` when multiple of 3 and 5', () => {
+    expect(FizzBuzz(30)).to.be.equal('FizzBuzz');
   })
 });
